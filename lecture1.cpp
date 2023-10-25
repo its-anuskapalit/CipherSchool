b5af2d7069e54f5f2a3a5521c1c6bc6bd19aa38d
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2023-10-25 19:19:46 +0530

    Create lecture1.cpp

---
+++ lecture1.cpp
@@ -0,0 +1,6 @@
+#include<iostream> //This is a header
+using namespace std;
+int main(){ //we have written a main function
+cout<<"Hello world"; //this will print Hello world on the screen
+return 0;
+}
