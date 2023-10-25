2638d6676aceea2d2e8b3c92c4f383f94dd32e56
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2023-10-25 19:21:59 +0530

    Create lecture3.cpp

---
+++ lecture3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //declare 2 variables assign them 3 & 5 and then add them
+     int a=3;
+     int b=5;
+
+     //a=3;
+     //b=5;
+
+     //int c;
+     //c=a+b;
+     //cout<<c;
+     cout<<"a+b:"<<endl<<a+b;
+}
